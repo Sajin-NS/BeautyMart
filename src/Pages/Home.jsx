9f3b18b0fc9393e52369eed884ae096d19a0fb0a
sajin sathyan <sajin.sathyan@realiti.io>
2024-11-20 18:55:50 +0530

    added home layout

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Layout } from "./Layout";
+import HomeLayout from "../Component/Home/HomeLayout"
+
+export const Home = () => {
+  return (
+    <Layout>
+      <HomeLayout />
+    </Layout>
+  );
+};
